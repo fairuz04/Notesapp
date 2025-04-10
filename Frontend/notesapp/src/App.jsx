1fec6d85bdfef403053471f66c2db05eb226ee51
fairuz04 <fairuz.suhala.reza@g.bracu.ac.bd>
2025-04-10 15:37:56 +0600

    Initial commit: frontend + backend scaffold

---
+++ Frontend/notesapp/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Home from './pages/Home/Home';
+
+const App = () => {
+  return (
+    <div>
+      <Home />
+    </div>
+  );
+}
+export default App;
+
+// export default function App() {
+//   return <Home />;
+// }
+
+// export default function App() {
+//   return <h1 style={{color: 'lime'}}>TEST‑123</h1>;
+// }
---
+++ Frontend/notesapp/src/pages/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+const Home = () => {
+  return (
+    <div>
+      Home
+    </div>
+  );
+}
+export default Home;
+// export default function Home() {
+//     return <h1>Hello from Home!</h1>;
+//   }
